09a5b0a40f052d069604e4d3ca07ecedc509f96b
Brijesh <brijeshprasad1989@gmail.com>
2020-11-01 13:35:33 +0100

    update README and change variable name

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Verivox assignment
+# Verivox assignment
+
+## Instructions to run the project
+
+1. yarn
+2. yarn start -- This will run the project on port 4000
+3. yarn test -- To run the test
+
+## API Documentation
+`/api/tariff?consumption=`
+Accepts - Consumption (kWh/year)
+Returns - List of Products in ascending order of their annual costs
+ 
--- src/api/routes/tariff.route.js
+++ src/api/routes/tariff.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const validate = require('express-validation');
 const paramValidation = require('../../config/param-validation');
-const userCtrl = require('../controllers/tariff.controller');
+const tariffController = require('../controllers/tariff.controller');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.route('/')
-  .get(validate(paramValidation.getTarrifs),userCtrl.get);
+  .get(validate(paramValidation.getTarrifs),tariffController.get);
 
 module.exports = router;
